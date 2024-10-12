142b67b60fa66d57ab5fcdecac3a2d9b8e0f749c
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2024-10-13 00:10:52 +0530

    minor changes in login page

--- auth/HTML/login1.php
+++ auth/HTML/login1.php
@@ -1,61 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel ="stylesheet" href="../CSS/login.css">
+    <link rel="stylesheet" href="../CSS/login.css">
 </head>
-<body>
-<div id="page-content" class="page-content">
-    <div class="banner">
-        <div class="jumbotron jumbotron-bg text-center rounded-0"
-            style="background-image: url('');">
-            <div class="container">
-                <h1 class="pt-5">
-                    Login Page
-                </h1>
-                <p class="lead">
-                    Save time and leave the groceries to us!
-                </p>
 
-                <div class="card card-login mb-5">
-                    <div class="card-body">
-                        <form class="form-horizontal" method="post" action="login.php">
-                            <div class="form-group row mt-3">
-                                <div class="col-md-12">
-                                    <input class="form-control" name="email" type="text" required=""
-                                        placeholder="Email">
+<body>
+    <div id="page-content" class="page-content">
+        <div class="banner">
+            <div class="jumbotron jumbotron-bg " style="background-image: url('');">
+                <div class="container">
+                    <h1 class="pt-5">
+                        Login Page
+                    </h1>
+                    <p class="lead">
+                        Save time and leave the groceries to us!
+                    </p>
+
+                    <div class="card card-login mb-5">
+                        <div class="card-body">
+                            <form class="form-horizontal" method="post" action="login.php">
+                                <div class="form-group row mt-3">
+                                    <div class="col-md-12">
+                                        <input class="form-control" name="email" type="text" required=""
+                                            placeholder="Email">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <div class="col-md-12">
-                                    <input class="form-control" name="password" type="password" required=""
-                                        placeholder="Password">
+                                <div class="form-group row">
+                                    <div class="col-md-12">
+                                        <input class="form-control" name="password" type="password" required=""
+                                            placeholder="Password">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <div class="col-md-12 d-flex justify-content-between align-items-center">
-                                  <div class="checkbox">
+                                <div class="form-group row">
+                                    <div class="col-md-12">
+                                        <div class="checkbox">
                                             <input id="checkbox0" type="checkbox" name="remember">
                                             <label for="checkbox0" class="mb-0"> Remember Me? </label><br>
                                         </div> <br>
-                                    <a href="login.html" class="text-light"><i class="fa fa-bell"></i> Forgot password?</a> 
+                                        <a href="login.html" class="text-light"><i class="fa fa-bell"></i> Forgot
+                                            password?</a>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group row text-center mt-4">
-                                <div class="col-md-12">
-                                    <button type="submit" name="submit"
-                                        class="btn btn-primary btn-block text-uppercase">Log
-                                        In</button>
+                                <div class="form-group row text-center mt-4">
+                                    <div class="col-md-12">
+                                        <button type="submit" name="submit" class="btn">Log
+                                            In</button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>  
 </body>
+
 </html>
