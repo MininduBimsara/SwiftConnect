87334080c8f7605a92233375b89588d482880d82
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2024-10-14 16:46:19 +0530

    small changes

--- auth/HTML/login1.php
+++ auth/HTML/login1.php
@@ -47,65 +47,63 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-   
+
     <link rel="stylesheet" href="../../includes/footer.css">
- <link rel="stylesheet" href="../CSS/login.css">
+    <link rel="stylesheet" href="../CSS/login.css">
 </head>
 
 <body>
-<div id="page-content" class="page-content">
-    <div class="banner">
-        <div class="jumbotron jumbotron-bg" style="background-image: url('');">
-            <div class="container">
-                <h1 class="pt-5">LOGIN</h1><br>
+    <div id="page-content" class="page-content">
+        <div class="banner">
+            <div class="jumbotron jumbotron-bg" style="background-image: url('');">
+                <div class="container">
+                    <h1 class="pt-5">LOGIN</h1><br>
 
-                <div class="card card-login mb-5">
-                    <div class="card-body">
-                        <form class="form-horizontal" method="post" action="login.php">
-                            <!-- Email Field -->
-                            <div class="form-group row mt-3">
-                                <div class="col-md-12">
-                                    <input class="form-control" name="email" type="text" required="" placeholder="Email">
-                                </div>
-                            </div>
-
-                            <!-- Password Field -->
-                            <div class="form-group row">
-                                <div class="col-md-12">
-                                    <input class="form-control" name="password" type="password" required="" placeholder="Password">
-                                </div>
-                            </div>
-
-                            <!-- Remember Me and Forgot Password -->
-                            <div class="form-group row remember-forgot">
-                                <div class="col-md-12 d-flex justify-content-between align-items-center">
-                                    <div class="checkbox">
-                                        <input id="checkbox0" type="checkbox" name="remember">
-                                        <label for="checkbox0" class="mb-0"> Remember Me? </label>
+                    <div class="card card-login mb-5">
+                        <div class="card-body">
+                            <form class="form-horizontal" method="post" action="login.php">
+                                <!-- Email Field -->
+                                <div class="form-group row mt-3">
+                                    <div class="col-md-12">
+                                        <input class="form-control" name="email" type="text" required=""
+                                            placeholder="Email">
                                     </div>
-                                    <a href="login.html" class="forgot-password-link">
-                                     <i class="fa fa-lock"></i> Forgot password?</a>
                                 </div>
-                            </div>
 
-                            <!-- Submit Button -->
-                            <div class="form-group row text-center mt-4">
-                                <div class="col-md-12">
-                                    <button type="submit" name="submit" class="btn btn-primary">LOGIN</button>
+                                <!-- Password Field -->
+                                <div class="form-group row">
+                                    <div class="col-md-12">
+                                        <input class="form-control" name="password" type="password" required=""
+                                            placeholder="Password">
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+
+                                <!-- Remember Me and Forgot Password -->
+                                <div class="form-group row remember-forgot">
+                                    <div class="col-md-12 d-flex justify-content-between align-items-center">
+                                        <div class="checkbox">
+                                            <input id="checkbox0" type="checkbox" name="remember">
+                                            <label for="checkbox0" class="mb-0"> Remember Me? </label>
+                                        </div>
+                                        <a href="login.html" class="forgot-password-link">
+                                            <i class="fa fa-lock"></i> Forgot password?</a>
+                                    </div>
+                                </div>
+
+                                <!-- Submit Button -->
+                                <div class="form-group row text-center mt-4">
+                                    <div class="col-md-12">
+                                        <button type="submit" name="submit" class="btn btn-primary">LOGIN</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-</body>
-
-</html>
-
-<?php include "../../includes/footer.php";
+    <?php include "../../includes/footer.php";
 ?>
