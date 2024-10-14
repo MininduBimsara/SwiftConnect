933a164bf4d89c1a76b875c87b8700c9b61e6236
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2024-10-14 18:48:42 +0530

    minor Changes

--- includes/header.php
+++ includes/header.php
@@ -18,6 +18,8 @@
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&display=swap" rel="stylesheet">
       <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+      <script src="https://kit.fontawesome.com/a295b70770.js" crossorigin="anonymous"></script>
 
   </head>
 
