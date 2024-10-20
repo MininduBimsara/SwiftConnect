2c90add94e4c6adc507299aee36176abb9541df9
Mohottige.S.D. <146794439+Sahani-Mohottige@users.noreply.github.com>
2024-10-20 15:25:46 +0530

    paypal Integration

--- package_management/paypal.php
+++ package_management/paypal.php
@@ -1,7 +1,5 @@
+<!-- This is a comment 
 
-<?php
-include "../includes/header.php";
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,9 +17,50 @@ include "../includes/header.php";
         <h2>Save time and leave the groceries to us!</h2>
         <button type="button" name="paypal" id="paypal">PayPal</button><br>
         <button type="button" name="credit" id="paypal">Debit or Credit Card</button>
+        
+        <div id="paypal-button-container"></div>
+        <p id="result-message"></p>
     </div><br>
 
+
+<script src="https://www.paypal.com/sdk/js?client-id=test&buyer-country=US&currency=USD&components=buttons&enable-funding=venmo,paylater,card"
+            data-sdk-integration-source="developer-studio"></script>
+        
+<script src="app.js"></script>
+-->
+<?php
+include "../includes/header.php";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>PayPal JS SDK Standard Integration</title>
+        <link rel="stylesheet" href="paypal.css">
+    <link rel="stylesheet" href="../includes/footer.css">
+    <link rel="stylesheet" href="../includes/header.css">
+
+    </head>
+    <body>
+
+    <h1>PAY WITH PAYPAL</h1>
+    <h2>Save time and leave the groceries to us!</h2>
+  
+        
+        <div id="paypal-button-container"></div>
+        <p id="result-message"></p>
+
+        
+        <!-- Initialize the JS-SDK -->
+        <script
+            src="https://www.paypal.com/sdk/js?client-id=test&buyer-country=US&currency=USD&components=buttons&enable-funding=venmo,paylater,card"
+            data-sdk-integration-source="developer-studio"
+        ></script>
+        <script src="app.js"></script>
+        
     <?php
 include "../includes/footer.php";
 ?>
-
+    </body>
+</html>
