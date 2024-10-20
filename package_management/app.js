fe29d72d8338d4aee8f00fbccb0480f4be18f30d
Mohottige.S.D. <146794439+Sahani-Mohottige@users.noreply.github.com>
2024-10-20 16:43:32 +0530

    changes

--- package_management/app.js
+++ package_management/app.js
@@ -6,6 +6,10 @@ window.paypal
             color: "gold",
             label: "paypal",
         },
+        message: {
+            amount: 100,
+        } ,
+
         async createOrder() {
             try {
                 const response = await fetch("/api/orders", {
@@ -13,6 +17,8 @@ window.paypal
                     headers: {
                         "Content-Type": "application/json",
                     },
+                    // use the "body" param to optionally pass additional order information
+                    // like product ids and quantities
                     body: JSON.stringify({
                         cart: [
                             {
@@ -28,7 +34,6 @@ window.paypal
                 if (orderData.id) {
                     return orderData.id;
                 }
-                
                 const errorDetail = orderData?.details?.[0];
                 const errorMessage = errorDetail
                     ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
@@ -36,36 +41,67 @@ window.paypal
 
                 throw new Error(errorMessage);
             } catch (error) {
-                console.error("Error creating order:", error);
+                console.error(error);
+                // resultMessage(`Could not initiate PayPal Checkout...<br><br>${error}`);
             }
-        },
+        } ,
+
         async onApprove(data, actions) {
             try {
-                const response = await fetch(`/api/orders/${data.orderID}/capture`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                });
+                const response = await fetch(
+                    `/api/orders/${data.orderID}/capture`,
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    }
+                );
 
                 const orderData = await response.json();
+                // Three cases to handle:
+                //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                //   (2) Other non-recoverable errors -> Show a failure message
+                //   (3) Successful transaction -> Show confirmation or thank you message
+
                 const errorDetail = orderData?.details?.[0];
 
                 if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+                    // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                    // recoverable state, per
+                    // https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
                     return actions.restart();
                 } else if (errorDetail) {
-                    throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
+                    // (2) Other non-recoverable errors -> Show a failure message
+                    throw new Error(
+                        `${errorDetail.description} (${orderData.debug_id})`
+                    );
                 } else if (!orderData.purchase_units) {
                     throw new Error(JSON.stringify(orderData));
                 } else {
+                    // (3) Successful transaction -> Show confirmation or thank you message
+                    // Or go to another URL:  actions.redirect('thank_you.html');
                     const transaction =
-                        orderData?.purchase_units?.[0]?.payments?.captures?.[0] ||
-                        orderData?.purchase_units?.[0]?.payments?.authorizations?.[0];
-                    console.log(`Transaction ${transaction.status}: ${transaction.id}`);
+                        orderData?.purchase_units?.[0]?.payments
+                            ?.captures?.[0] ||
+                        orderData?.purchase_units?.[0]?.payments
+                            ?.authorizations?.[0];
+                    resultMessage(
+                        `Transaction ${transaction.status}: ${transaction.id}<br>
+          <br>See console for all available details`
+                    );
+                    console.log(
+                        "Capture result",
+                        orderData,
+                        JSON.stringify(orderData, null, 2)
+                    );
                 }
             } catch (error) {
-                console.error("Error processing order:", error);
+                console.error(error);
+                resultMessage(
+                    `Sorry, your transaction could not be processed...<br><br>${error}`
+                );
             }
-        },
+        } ,
     })
-    .render("#paypal-button-container");
+    .render("#paypal-button-container"); 
