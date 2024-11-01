43746af24d34d50c6078fe8b2eac0e938dbbb2c4
holibhai <mohanathasholins17@gmail.com>
2024-11-01 08:05:24 +0530

    minor changes

--- package_management/delivery_details.php
+++ package_management/delivery_details.php
@@ -51,9 +51,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Package Management</title>
         <link rel="stylesheet" href="../includes/header.css">
-        <!-- <link rel="stylesheet" href="../includes/footer.css"> -->
-        <link rel="stylesheet" href="../includes/footer.css?v=1.0">
-        <link rel="stylesheet" href="new1.css">
+        <link rel="stylesheet" href="../includes/footer.css">
+        <link rel="stylesheet" href="delivery_details.css">
+    
     </head>
 
     <body>
